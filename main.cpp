cb88bb2b1ae227672b009e4518cb4bee1063559e
sacha <sacha.laurent71@gmail.com>
2025-11-18 16:26:07 +0100

    Feat: update main.cpp to enhance output formatting and add swipe functionality

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-#include "Model/Case.h"
 #include "Model/Direction.h"
 #include "Model/Grille.h"
 
@@ -8,7 +7,11 @@ int main() {
 
     Grille g(5, 5);
 
-    std::cout << g;
+    std::cout << g << std::endl;
+
+    g.swipe(Direction::Right);
+
+    std::cout << g << std::endl;
 
     return 0;
 }
