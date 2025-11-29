6edba10527884423b9d415c38d3f1bb4557b4c8a
CasahDev <sacha.laurent71@gmail.com>
2025-11-29 23:42:14 +0100

    Feat: implement real-time input handling and game over display in main loop

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-#include "Model/Direction.h"
 #include "Model/Game.h"
-#include "Model/Grille.h"
 #include "Vue/ConsoleVue.h"
+#include <unistd.h>     // STDIN_FILENO
+#include <termios.h>    // struct termios, tcgetattr, tcsetattr, etc.
+#include <stdio.h>
 
 int main() {
 
@@ -13,9 +14,54 @@ int main() {
 
     ConsoleVue vue(game_vm);
 
-    vue.Display();
+    vue.display();
+    bool lost = false;
 
-    vue.move(Direction::Left);
-    vue.Display();
+    termios t{};
+
+    if (tcgetattr(STDIN_FILENO, &t) == -1) {
+        perror("tcgetattr");
+        return -1;
+    }
+
+    t.c_lflag &= ~ICANON;
+    t.c_lflag &= ~ECHO;
+
+    if (tcsetattr(STDIN_FILENO, TCSANOW, &t) == -1) {
+        perror("tcsetattr");
+    }
+
+    while (!lost) {
+        char key = 0;
+        if (read(STDIN_FILENO, &key, 1) == 1) {
+            std::optional<Direction> direction;
+
+            switch (key) {
+                case 'z':
+                    direction = Direction::Up;
+                    break;
+                case 'q':
+                    direction = Direction::Left;
+                    break;
+                case 's':
+                    direction = Direction::Down;
+                    break;
+                case 'd':
+                    direction = Direction::Right;
+                    break;
+            }
+
+            if (direction.has_value()) {
+                vue.move(direction.value());
+
+                vue.display();
+
+                lost = vue.check_for_loose();
+            }
+        }
+    }
+
+    vue.display_lost();
+
     return 0;
 }
