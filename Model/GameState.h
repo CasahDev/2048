d8474e30a3be61c68cbef164598686a4ba1fa57c
CasahDev <sacha.laurent71@gmail.com>
2025-11-30 00:31:34 +0100

    Feat: add GameState enum to represent different game states

---
+++ Model/GameState.h
@@ -0,0 +1,14 @@
+//
+// Created by sacha on 30/11/2025.
+//
+
+#ifndef TD2_GAMESTATE_H
+#define TD2_GAMESTATE_H
+
+enum class GameState {
+    Pending,
+    Won,
+    Lost
+};
+
+#endif //TD2_GAMESTATE_H
